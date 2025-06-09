2165cec5ccb048cc2439f81c5cbd046dfb40593d
Anthony Shaga <antonyshaga@gmail.com>
2025-06-10 00:20:27 +0300

    refactor: SideMenu

--- src/lib/auth/getServerAuthSession.ts
+++ src/lib/auth/getServerAuthSession.ts
@@ -4,11 +4,11 @@ import { verifyToken } from "@/lib/auth/verifyToken";
 export async function getServerAuthSession() {
   const cookieStore = await cookies();
   const token = cookieStore.get("token")?.value;
-  console.log(token);
+
   if (!token) return null;
 
   try {
-    const payload = await verifyToken(token); // { id: "...", email: "...", ... }
+    const payload = await verifyToken(token);
     console.log(payload);
     return { user: payload }; // session.user.id
   } catch {
--- src/lib/auth/verifyToken.ts
+++ src/lib/auth/verifyToken.ts
@@ -1,12 +1,7 @@
-import jwt from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
 
 const JWT_SECRET = process.env.JWT_SECRET!;
 
-export function verifyToken(token: string) {
-  return new Promise<any>((resolve, reject) => {
-    jwt.verify(token, JWT_SECRET, (err, decoded) => {
-      if (err) reject(err);
-      else resolve(decoded);
-    });
-  });
+export function verifyToken(token: string): JwtPayload {
+  return jwt.verify(token, JWT_SECRET) as JwtPayload;
 }
