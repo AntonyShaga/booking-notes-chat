c7da97da10cb3579cb5cc170019afb978f3bc1f5
Anthony Shaga <antonyshaga@gmail.com>
2025-06-11 18:17:44 +0300

    feat: implement 2FA setup components: CoopyIcon

--- src/components/settings/2FA/enable2FA/ManualSecretSection.tsx
+++ src/components/settings/2FA/enable2FA/ManualSecretSection.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { motion } from "framer-motion";
 import { toast } from "sonner";
+import CopyIcon from "@/components/icons/CopyIcon";
+import Button from "@/components/ui/Button";
 
 export default function ManualSecretSection({ secret }: { secret: string | null }) {
   const copyToClipboard = () => {
@@ -24,26 +26,13 @@ export default function ManualSecretSection({ secret }: { secret: string | null 
         <code className="bg-gray-100 p-2 block rounded break-all flex-1">
           {secret || "••••••••••••••••••••••••••••"}
         </code>
-        <button
+        <Button
           onClick={copyToClipboard}
           className="p-2 bg-gray-200 rounded hover:bg-gray-300"
           title="Копировать"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect>
-            <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path>
-          </svg>
-        </button>
+          <CopyIcon />
+        </Button>
       </div>
     </motion.div>
   );
