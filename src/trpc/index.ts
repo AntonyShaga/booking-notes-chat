4a4972861832fc44f586671cbe95d11239929db4
Anthony Shaga <antonyshaga@gmail.com>
2025-07-25 03:14:56 +0300

    delete: getEmaileStatusRout

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -3,7 +3,6 @@ import { router } from "./trpc";
 import { registerRouter } from "@/trpc/routers/register";
 import { verifyEmailRouter } from "@/trpc/routers/verifyEmail";
 import { logoutRouter } from "@/trpc/routers/logoutRouter";
-import { getEmailStatusRout } from "@/trpc/routers/getEmailStatus";
 import { twoFARouter } from "@/trpc/routers/2fa/twoFARouter";
 import { authRouter } from "@/trpc/routers/auth/auth";
 
@@ -12,7 +11,6 @@ export const appRouter = router({
   register: registerRouter,
   verifyEmail: verifyEmailRouter,
   logout: logoutRouter,
-  getEmailStatus: getEmailStatusRout,
   twoFA: twoFARouter,
 });
 
